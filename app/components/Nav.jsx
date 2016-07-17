2a8a342c6745d3c6c0eaa9d3b3be77ae0d824507
Eric Hodges <ehodges2004@gmail.com>
2016-07-17 14:10:33 -0700

    Added nav bar names and worked on responsive nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,33 +6,35 @@ var Nav = React.createClass({
   render: function() {
     return (
       <div className="top-bar">
+        <div className="top-bar-title">
+          <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+            <button className="menu-icon dark" type="button" data-toggle></button>
+          </span>      
+        </div>
+        <div id="responsive-menu">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">React Weather App
-            </li>
             <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Work</IndexLink>
             </li>
             <li>
               <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
             </li>
             <li>
-              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Résumé</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>GitHub</Link>
             </li>
           </ul>
         </div>
         <div className="top-bar-right">
-          <form onSubmit={this.onSearch}>
             <ul className="menu">
-              <li>
-                <input type="search" placeholder="Search weather by city" ref="search"/>
-              </li>
-              <li>
-                <input type="submit" className="button" value="Get Weather"/>
+              <li className="menu-text">Eric Hodges
               </li>
             </ul>
-          </form>
         </div>
+      </div>
       </div>
     );
   }
